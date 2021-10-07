8eabab1493df5e101ca57293b633c62dae22c232
alettin <alaeddincetin@hotmail.com>
2021-10-07 15:21:28 +0200

    MVC Routing
    This commit is about how the MVC route system works. You can add your route configuration. Also you can add it with parameters.

--- MVC_WebApplication/MVC_WebApplication/App_Start/RouteConfig.cs
+++ MVC_WebApplication/MVC_WebApplication/App_Start/RouteConfig.cs
@@ -13,11 +13,28 @@ namespace MVC_WebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "Articles",
+                url: "article",
+                defaults: new { controller = "Article", action = "Index" },
+                namespaces: new[] { "MVC_WebApplication.Controllers" }
+                );
+
+            routes.MapRoute(
+                name: "Events",
+                url: "event",
+                defaults: new { controller = "Event", action = "Index",id = 99 },
+                namespaces: new[] { "MVC_WebApplication.Controllers" }
+                );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+           );
+
+            routes.MapMvcAttributeRoutes();
         }
     }
 }
