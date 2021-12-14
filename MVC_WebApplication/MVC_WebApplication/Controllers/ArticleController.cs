f9b4248dcacf5ef90b728b8ae7db2b2396a7be3c
alettin <alaeddincetin@gmail.com>
2021-12-14 23:33:04 +0100

    Other Files
    Other Files

--- MVC_WebApplication/MVC_WebApplication/Controllers/ArticleController.cs
+++ MVC_WebApplication/MVC_WebApplication/Controllers/ArticleController.cs
@@ -1,6 +1,8 @@
 ﻿using MVC_WebApplication.Models;
+using MVC_WebApplication.Models.DatabaseModel;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,29 +11,54 @@ namespace MVC_WebApplication.Controllers
 {
     public class ArticleController : Controller
     {
+      
+
         public ActionResult Index()
         {
-            return View();
+            dbContext dbContext = new dbContext {_mvcProjectEntities = new MvcProjectEntities()};
+            var model = dbContext._mvcProjectEntities.Articles.OrderBy(x=> x.CreateDate).ThenByDescending(x => x.CreateDate);
+            return View(model);
         }
 
         public ActionResult ArticleDetails(int ItemId)
         {
-            var model = "Get article detail from database with parameter";
-            return View("~/Views/Article/ArticleDetails.cshtml", model: model);
+            dbContext dbContext = new dbContext { _mvcProjectEntities = new MvcProjectEntities() };
+            var model = dbContext._mvcProjectEntities.Articles.Find(ItemId);
+            //var model = "Get article detail from database with parameter";
+            //return View("~/Views/Article/ArticleDetails.cshtml", model: model);
+            if (model == null)
+                return RedirectToAction("PageNotFound","Error");
+            return View(model);
         }
 
-        public ActionResult CreateArticle()
+        public ActionResult CreateOrUpdateArticle(int? id)
         {
-            var model = new vmArticle();
+            var model = new Article();
+
+            if(Convert.ToInt32(id) > 0)
+            {
+                dbContext dbContext = new dbContext { _mvcProjectEntities = new MvcProjectEntities() };
+                model = dbContext._mvcProjectEntities.Articles.Find(id);
+            }
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult CreateArticle(vmArticle vmArticle)
+        public ActionResult CreateOrUpdateArticle(Article article)
         {
-            //save to database then redirect the page
-            TempData["model"] = vmArticle;
-            return RedirectToAction("Details");
+            dbContext dbContext = new dbContext { _mvcProjectEntities = new MvcProjectEntities() };
+            var model = article;
+            model.CreateDate = DateTime.Now;
+            if (article.Id > 0)
+            {
+                dbContext._mvcProjectEntities.Articles.Attach(model);
+                dbContext._mvcProjectEntities.Entry(model).State = EntityState.Modified;
+            }
+            else
+                dbContext._mvcProjectEntities.Articles.Add(model);
+
+            dbContext._mvcProjectEntities.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public ActionResult Details()
