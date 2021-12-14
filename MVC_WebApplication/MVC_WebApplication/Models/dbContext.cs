06c160ffa85056845775a094acb0d781a7ad9080
alettin <alaeddincetin@gmail.com>
2021-12-14 23:54:49 +0100

    Database Operations
    Database Operations with Ado.net Entity Data Model (Add, Update and List)

---
+++ MVC_WebApplication/MVC_WebApplication/Models/dbContext.cs
@@ -0,0 +1,13 @@
+﻿using MVC_WebApplication.Models.DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WebApplication.Models
+{
+    public class dbContext
+    {
+        public MvcProjectEntities _mvcProjectEntities { get; set; }
+    }
+}
