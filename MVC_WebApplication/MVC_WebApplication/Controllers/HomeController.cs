caba30bfe4d4b65f7db631fedd804e62deae5abc
alettin <alaeddincetin@gmail.com>
2021-12-14 23:36:41 +0100

    Revert "Other Files"
    This reverts commit f9b4248dcacf5ef90b728b8ae7db2b2396a7be3c.

--- MVC_WebApplication/MVC_WebApplication/Controllers/HomeController.cs
+++ MVC_WebApplication/MVC_WebApplication/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using MVC_WebApplication.Models;
-using MVC_WebApplication.Models.DatabaseModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,42 +25,6 @@ namespace MVC_WebApplication.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        public ActionResult CategoryList()
-        {
-            dbContext dbcontext = new dbContext { _mvcProjectEntities = new MvcProjectEntities() };
-            var model = dbcontext._mvcProjectEntities.Categories.ToList().OrderBy(x => x.DisplayOrder);
-            return View(model);
-        }
-
-        public ActionResult CreateOrUpdateCategory(int? id)
-        {
-            var model = new Category();
-            if (Convert.ToInt32(id) > 0)
-            {
-                dbContext dbcontext = new dbContext { _mvcProjectEntities = new MvcProjectEntities() };
-                model = dbcontext._mvcProjectEntities.Categories.Find(id);
-            }
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult CreateOrUpdateCategory(Category category)
-        {
-            dbContext dbcontext = new dbContext { _mvcProjectEntities = new MvcProjectEntities() };
-            var model = category;
-            model.CreateDate = DateTime.Now;
-            if ((model.Id) > 0)
-            {
-                dbcontext._mvcProjectEntities.Categories.Attach(model);
-                dbcontext._mvcProjectEntities.Entry(model).State = EntityState.Modified;
-            }
-            else
-                dbcontext._mvcProjectEntities.Categories.Add(model);
-            dbcontext._mvcProjectEntities.SaveChanges();
-
-            return RedirectToAction("CategoryList");
         }
     }
 }
